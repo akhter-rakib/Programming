6f836d1b93acd9e42b57f1cedb39b7598c299e93
Aktaruzzaman Rakib <rakibccj@gmail.com>
2017-07-28 13:23:14 +0600

    Odd Even Program Taking value from user

--- src/OddEvenProgram.java
+++ src/OddEvenProgram.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 /**
  * Created by Rakib on 7/28/2017.
  */
 public class OddEvenProgram {
     public static void main(String[] args) {
-        
+        Scanner numOne = new Scanner(System.in);
+        System.out.println("Give Ur First No : ");
+        int a = numOne.nextInt();
+        if (a % 2 == 0) {
+            System.out.println("ur Guven Value" + a + "is a Even Number ");
+        } else {
+            System.out.println("Ur value is" + a + "Odd Value ");
+        }
     }
 }
