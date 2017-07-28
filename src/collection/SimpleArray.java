13ba201b0159e8711f0a4a53d8118845ff1d703c
Aktaruzzaman Rakib <rakibccj@gmail.com>
2017-07-28 15:19:26 +0600

    Collection Framework

---
+++ src/collection/SimpleArray.java
@@ -0,0 +1,13 @@
+package collection;
+//An arraY is index collection of  fixed number  of homogenious data element
+//Main advantages of array is we can hold multiple values within a single variable
+//advantags: it holds only homogenious element
+public class SimpleArray {
+
+    public static void main(String[] args) {
+        int a[] = {1, 2, 4};
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
