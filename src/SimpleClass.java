4db235179183a5c5228afd7ea7284e0ed767c994
Aktaruzzaman Rakib <rakibccj@gmail.com>
2017-07-28 12:56:22 +0600

    first commit

---
+++ src/SimpleClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Rakib on 7/28/2017.
+ */
+public class SimpleClass {
+
+    public static void main(String[] args) {
+        System.out.println("You are  in the line ");
+    }
+}
