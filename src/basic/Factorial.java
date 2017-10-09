a7be913f796d8bc8c0497e5d2f20758c570f1afe
ATUAR-PC\Atuar <atuar.avis@gmail.com>
2017-10-09 16:18:50 +0600

    Factorial Calculation

---
+++ src/basic/Factorial.java
@@ -0,0 +1,23 @@
+package basic;
+
+import java.util.Scanner;
+
+/**
+ * Created by Atuar on 10/9/2017.
+ */
+public class Factorial {
+    public static void main(String[] args) {
+        int i, number, fact = 1;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your number: ");
+        number = input.nextInt();
+
+        for (i =1; i <= number; i++){
+            fact = fact*i;
+        }
+        System.out.println("Factorial of "+number+ " is: "+fact);
+    }
+
+}
