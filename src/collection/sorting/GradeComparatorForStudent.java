41f106ae18b099136f557518111ea092759f99a6
Aktaruzzaman Rakib <rakibccj@gmail.com>
2017-10-16 11:50:06 +0600

    Comparator Class Update

--- src/collection/sorting/GradeComparatorForStudent.java
+++ src/collection/sorting/GradeComparatorForStudent.java
@@ -3,11 +3,8 @@ package collection.sorting;
 import java.util.Comparator;
 
 public class GradeComparatorForStudent implements Comparator<Student> {
-
-
     @Override
     public int compare(Student o1, Student o2) {
         return o1.getName().compareTo(o2.getName());
-        //return o1.getGrade() - o2.getGrade();
     }
 }
