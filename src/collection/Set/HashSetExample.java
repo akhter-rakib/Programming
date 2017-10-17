bb88fbd1ae2c6b121bfb19102d7cceb71a2ff8bf
Aktaruzzaman Rakib <rakibccj@gmail.com>
2017-10-17 12:44:52 +0600

    Set interface

---
+++ src/collection/Set/HashSetExample.java
@@ -0,0 +1,25 @@
+package collection.Set;
+
+import java.util.HashSet;
+//dublicate value is not allowed
+public class HashSetExample {
+    public static void main(String args[]) {
+        // HashSet declaration
+        HashSet<String> hset = new HashSet<String>();
+
+        // Adding elements to the HashSet
+        hset.add("Apple");
+        hset.add("Mango");
+        hset.add("Grapes");
+        hset.add("Orange");
+        hset.add("Fig");
+        //Addition of duplicate elements
+        hset.add("Apple");
+        hset.add("Mango");
+        //Addition of null values
+        hset.add(null);
+        hset.add(null);
+        //Displaying HashSet elements
+        System.out.println(hset);
+    }
+}
