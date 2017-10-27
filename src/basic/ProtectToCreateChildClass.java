ea6a000db85163c1c3c14ea5d7b2b00e6bd31958
Aktaruzzaman Rakib <rakibccj@gmail.com>
2017-10-27 22:16:34 +0600

    Protected to create child class

--- src/basic/ProtectToCreateChildClass.java
+++ src/basic/ProtectToCreateChildClass.java
@@ -2,7 +2,7 @@ package basic;
 
 public class ProtectToCreateChildClass {
     private ProtectToCreateChildClass() {
-        //ae class ar constrcutor private korar karone ae class ar child class create kora possible noi 
+        //ae class ar constrcutor private korar karone ae class ar child class create kora possible noi
 
     }
 }
